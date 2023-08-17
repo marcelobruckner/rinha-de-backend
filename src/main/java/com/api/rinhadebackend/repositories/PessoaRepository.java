08516cf6e519dee96b21fc5ac0e268d4292b3e28
Marcelo Bruckner <marcelobruckner@gmail.com>
2023-08-17 10:12:32 -0300

    formatando melhor a classe

--- src/main/java/com/api/rinhadebackend/repositories/PessoaRepository.java
+++ src/main/java/com/api/rinhadebackend/repositories/PessoaRepository.java
@@ -10,11 +10,10 @@ import com.api.rinhadebackend.models.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, UUID> {
 
-  @Query(nativeQuery = true, value = "select  distinct p.* from pessoas p " +
-      "left join pessoas_stacks ps on (p.id = ps.pessoa_id) " +
-      "where lower(p.apelido) LIKE lower(concat('%', :searchTerm , '%')) " +
-      "or lower(p.nome) LIKE lower(concat('%', :searchTerm , '%')) " +
-      "or lower(ps.stack) LIKE lower(concat('%', :searchTerm , '%')) " +
-      "limit 50")
-  List<Pessoa> findAllBySearchTerm(String searchTerm);
+	@Query(nativeQuery = true, value = "select  distinct p.* from pessoas p "
+			+ "left join pessoas_stacks ps on (p.id = ps.pessoa_id) "
+			+ "where lower(p.apelido) LIKE lower(concat('%', :searchTerm , '%')) "
+			+ "or lower(p.nome) LIKE lower(concat('%', :searchTerm , '%')) "
+			+ "or lower(ps.stack) LIKE lower(concat('%', :searchTerm , '%')) " + "limit 50")
+	List<Pessoa> findAllBySearchTerm(String searchTerm);
 }
